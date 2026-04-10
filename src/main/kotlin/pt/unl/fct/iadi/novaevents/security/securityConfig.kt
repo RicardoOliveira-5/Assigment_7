7e18e5ae1163cdcc704a8b5296390d05df125be5
RicardoOliveira-5 <a49436@alunos.isel.pt>
2026-04-10 15:47:30 +0100

    new12

--- src/main/kotlin/pt/unl/fct/iadi/novaevents/security/securityConfig.kt
+++ src/main/kotlin/pt/unl/fct/iadi/novaevents/security/securityConfig.kt
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
@@ -42,6 +43,9 @@ class SecurityConfig (
             .csrf { csrf ->
                 csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 csrf.csrfTokenRequestHandler(CsrfTokenRequestAttributeHandler())
+            }
+            .exceptionHandling {
+                it.authenticationEntryPoint(LoginUrlAuthenticationEntryPoint("/login"))
             }
             .authorizeHttpRequests { auth ->
                 auth
